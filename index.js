72b235d4082b9cbd3e23eb3c3528a4e18b0e5e93
Preston Pham <prestonp08@gmail.com>
2015-02-18 10:28:49 -0600

    Use entry message by default

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var rollbar = require('rollbar');
 module.exports = function(options) {
   options = options || {};
   options.levels = options.levels || ['error']
-  options.msg = options.msg || 'Loglog Entry';
   rollbar.init(options.accessToken);
   
   return function handleEntry(entry) {
     if (options.levels.indexOf(entry.level) === -1) return;
-    rollbar.reportMessageWithPayloadData(options.msg, entry);
+    rollbar.reportMessageWithPayloadData(options.message || entry.message, entry);
   };
 };
